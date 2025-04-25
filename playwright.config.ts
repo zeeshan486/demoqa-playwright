54e4b6de04ebd75dae2cb0e94951523476fd52cb
zeeshan486 <zeeshanalisyed51@gmail.com>
2025-04-25 15:18:51 +0530

    remove some file

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: 'https://demoqa.com',
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
